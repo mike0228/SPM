c3cc0dbbc9f9f4e67b1b3eb125974a955b5787f8
mike0228 <mike990228@gmail.com>
2019-12-09 22:05:10 +0800

    initialize

---
+++ src/main/java/cn/edu/njust/dev/ses/main/MainApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.dev.ses.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
