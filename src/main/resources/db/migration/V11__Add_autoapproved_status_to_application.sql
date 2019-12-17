ba2d09a5c3ed6246077e3fc13b9409cb664532d5
UShimamura <hzy980512@126.com>
2019-12-17 20:50:23 +0800

    Altered database.

--- src/main/resources/db/migration/V11__Add_autoapproved_status_to_application.sql
+++ src/main/resources/db/migration/V11__Add_autoapproved_status_to_application.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `spm_assignment`.`application`
-    CHANGE COLUMN `app_status` `app_status` ENUM('not confirmed', 'pending', 'auto-approved', 'approved', 'failed') NOT NULL DEFAULT 'pending' ;
+    CHANGE COLUMN `app_status` `app_status` ENUM('not confirmed', 'pending', 'auto-approved', 'approved', 'manually-approved', 'failed') NOT NULL DEFAULT 'pending' ;
