24869a8c19c9b999277e019169827b7b06a5332d
driftwoodu <18851072662@163.com>
2019-12-18 22:46:27 +0800

    inspect

--- src/main/java/cn/edu/njust/dev/ses/main/controller/student/ExportExcelController.java
+++ src/main/java/cn/edu/njust/dev/ses/main/controller/student/ExportExcelController.java
@@ -1,4 +1,58 @@
 package cn.edu.njust.dev.ses.main.controller.student;
 
+import cn.edu.njust.dev.ses.main.dto.StudentDTO;
+import cn.edu.njust.dev.ses.main.service.ExportExcelService;
+import org.apache.poi.hssf.usermodel.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
 public class ExportExcelController {
+    @Autowired
+    private ExportExcelService exportExcelService;
+    @RequestMapping(value = "/UserExcelDownloads", method = RequestMethod.GET)
+    public void downloadAllClassmate(HttpServletResponse response) throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("信息表");
+
+        List<StudentDTO> studentDTOS = exportExcelService.studentDTOS();
+
+        String fileName = "userinf"  + ".xls";//设置要导出的文件的名字
+        //新增数据行，并且设置单元格数据
+
+        int rowNum = 1;
+
+        String[] headers = { "姓名", "身份证号"};
+        //headers表示excel表中第一行的表头
+
+        HSSFRow row = sheet.createRow(0);
+        //在excel表中添加表头
+
+        for(int i=0;i<headers.length;i++){
+            HSSFCell cell = row.createCell(i);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            cell.setCellValue(text);
+        }
+
+        //在表中存放查询到的数据放入对应的列
+        for (StudentDTO studentDTO : studentDTOS) {
+            HSSFRow row1 = sheet.createRow(rowNum);
+            row1.createCell(0).setCellValue(studentDTO.getName());
+            row1.createCell(1).setCellValue(studentDTO.getIdNo());
+            rowNum++;
+        }
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+        response.flushBuffer();
+        workbook.write(response.getOutputStream());
+    }
+
+
 }
--- src/main/java/cn/edu/njust/dev/ses/main/service/ExportExcelService.java
+++ src/main/java/cn/edu/njust/dev/ses/main/service/ExportExcelService.java
@@ -1,43 +1,31 @@
 package cn.edu.njust.dev.ses.main.service;
 
-import com.haue.jobonline.dao.CompetitionSignUpDao;
-import com.haue.jobonline.entity.CompetitionSignUp;
-import com.haue.jobonline.service.SignUpService;
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.xssf.usermodel.*;
+import cn.edu.njust.dev.ses.main.dto.StudentDTO;
+import cn.edu.njust.dev.ses.main.mapper.StudentMapper;
+import cn.edu.njust.dev.ses.main.model.Student;
+import cn.edu.njust.dev.ses.main.model.StudentExample;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service
-public class ExportExcel {
-    @Resource
-    private CompetitionSignUpDao competitionSignUpDao;
-    public List<Map<String, Object>> createExcelRecord(List<CompetitionSignUp> competitionSignUps) {
-        List<Map<String, Object>> listmap = new ArrayList<Map<String, Object>>();
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("sheetName", "sheet1");
-        listmap.add(map);
-        for (int j = 0; j < competitionSignUps.size(); j++) {
-            CompetitionSignUp competitionSignUp=competitionSignUps.get(j);
-            Map<String, Object> mapValue = new HashMap<String, Object>();
-//            String keys[]    =     {"studentNumber","studentName","studentSex","studentSpecialy","studentClass","time"};//map中的key
-            mapValue.put("studentNumber", competitionSignUp.getStudent().getStudentSchoolNum());
-            mapValue.put("studentName",competitionSignUp.getStudent().getStudentName());
-            mapValue.put("studentSex",competitionSignUp.getStudent().getStudentSex());
-            mapValue.put("studentSpecialy",competitionSignUp.getStudent().getStudentSpecialty());
-            mapValue.put("studentClass",competitionSignUp.getStudent().getStudentClass());
-            mapValue.put("time",competitionSignUp.getSignTime());
-            listmap.add(mapValue);
-        }
-        return listmap;
+public class ExportExcelService {
+    @Autowired
+    private StudentMapper studentMapper;
+    public List<StudentDTO> studentDTOS(){
+        StudentExample example = new StudentExample();
+        StudentExample.Criteria criteria = example.createCriteria();
+        List<Student> students = studentMapper.selectByExample(example);
+        List<StudentDTO> studentDTOS = students.stream().map(q->{
+            StudentDTO studentDTO = new StudentDTO();
+            BeanUtils.copyProperties(q,studentDTO);
+            return studentDTO;
+        }).collect(Collectors.toList());
+        return studentDTOS;
     }
-
-
 }
 
