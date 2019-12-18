82d1201f4a2ad15e444c6df08fc9717d414dd27a
08mamba24 <864701928@qq.com>
2019-12-18 22:18:32 +0800

    fix the format SelectRankEntryDTO

--- src/main/java/cn/edu/njust/dev/ses/main/dto/SelectRankEntryDTO.java
+++ src/main/java/cn/edu/njust/dev/ses/main/dto/SelectRankEntryDTO.java
@@ -7,10 +7,19 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class SelectRankEntryDTO  extends GenericDTO {
-
+    @ExcelField("序号")
     private Integer reid;
-    private Integer uid;
-    @ExcelField("选拔排名")
+    @ExcelField("姓名")
+    private String name;
+    @ExcelField("学号")
+    private String studentId;
+//    @ExcelField("证件号码")
+//    private String idNo;
+    @ExcelField("年级")
+    private Integer grade;
+    @ExcelField("身份证号码")
+    private String idNo;
+    @ExcelField("选拔赛rank")
     private Integer rank;
-    private Integer eid;
+    private Integer eid;//导入的时候填写
 }
