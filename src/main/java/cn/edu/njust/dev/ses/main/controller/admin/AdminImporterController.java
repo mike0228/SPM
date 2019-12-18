d47ea4697a1e930dbd7042fbe840c0e12cbb1a86
08mamba24 <864701928@qq.com>
2019-12-19 01:48:45 +0800

    completed import and confirm SelectRankEntry create SelectRankEntryDTO

--- src/main/java/cn/edu/njust/dev/ses/main/controller/admin/AdminImporterController.java
+++ src/main/java/cn/edu/njust/dev/ses/main/controller/admin/AdminImporterController.java
@@ -2,11 +2,9 @@ package cn.edu.njust.dev.ses.main.controller.admin;
 
 import cn.edu.njust.dev.ses.main.dto.GradesEntryDTO;
 import cn.edu.njust.dev.ses.main.dto.ResultDTO;
+import cn.edu.njust.dev.ses.main.dto.SelectRankEntryDTO;
 import cn.edu.njust.dev.ses.main.dto.StudentDTO;
-import cn.edu.njust.dev.ses.main.mapper.ApplicationMapper;
-import cn.edu.njust.dev.ses.main.mapper.CCFEventMapper;
-import cn.edu.njust.dev.ses.main.mapper.GradesEntryMapper;
-import cn.edu.njust.dev.ses.main.mapper.StudentMapper;
+import cn.edu.njust.dev.ses.main.mapper.*;
 import cn.edu.njust.dev.ses.main.model.*;
 import cn.edu.njust.dev.ses.main.service.StudentService;
 import cn.edu.njust.dev.ses.main.util.TempStorageObject;
@@ -41,6 +39,8 @@ public class AdminImporterController {
     GradesEntryMapper gradesEntryMapper;
     @Autowired
     StudentService studentService;
+    @Autowired
+    SelectRankEntryMapper selectRankEntryMapper;
 
     @ResponseBody
     @RequestMapping("/api/json/import_student_via_excel")
@@ -86,7 +86,7 @@ public class AdminImporterController {
             return ResultDTO.errorOf(0, String.format("发生异常（%s）", ex.getClass()), ex.getLocalizedMessage());
         }
 
-        for(GradesEntryDTO gradesEntryDTO: result){
+        for(GradesEntryDTO gradesEntryDTO: result){//根据证件号将考试成绩与学生表连起来，获得学生ID
             StudentExample studentExample = new StudentExample();
             studentExample.createCriteria().andIdNoEqualTo(gradesEntryDTO.getIdNo());
             List<Student> students = studentMapper.selectByExample(studentExample);
@@ -106,24 +106,64 @@ public class AdminImporterController {
 
     @ResponseBody
     @RequestMapping("/api/json/import_select_ranks_via_excel")
-    public ResultDTO importSelectRankEntryThroughExcel(HttpSession session, @RequestParam MultipartFile file){
+    public ResultDTO importSelectRankEntryThroughExcel(HttpSession session, @RequestParam MultipartFile file,
+                                                       @RequestParam Integer eid/*SelectRank id*/){
         User sessionUser = (User) session.getAttribute("logged_in_as");
         if(sessionUser == null|| !sessionUser.getType().equals("associate")){
             return ResultDTO.errorOf(0, "用户未登录或用户类型不正确。");
         }
         //TODO
-        return null;
+        //注：上传学生选拔考试的成绩
+        List<SelectRankEntryDTO> result = null;
+        try{
+            InputStream in = file.getInputStream();
+            ExcelUniversalParser<SelectRankEntryDTO> eup = new ExcelUniversalParser<>();
+            result = eup.parseFrom(in, SelectRankEntryDTO.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResultDTO.errorOf(0, String.format("发生异常（%s）", ex.getClass()), ex.getLocalizedMessage());
+        }
+
+        for(SelectRankEntryDTO selectRankEntryDTO: result){
+            StudentExample studentExample = new StudentExample();
+            studentExample.createCriteria().andIdNoEqualTo(selectRankEntryDTO.getIdNo());
+            List<Student> students = studentMapper.selectByExample(studentExample);
+            if(!students.isEmpty())
+                selectRankEntryDTO.setStudentId(students.get(0).getStudentId());
+        }
+
+
+        TempStorageObject<List<SelectRankEntryDTO>> tempStorageObject = new TempStorageObject<>();
+        tempStorageObject.setData(result);
+        tempStorageObject.setCreationDate(new Date());
+
+        tempStorageObject.setId(RandomUtils.nextInt());
+        session.setAttribute("last_result_select_ranks_import", tempStorageObject);
+        return ResultDTO.okOf(result);
     }
 
     @ResponseBody
     @RequestMapping("/api/json/confirm_rank_import")
-    public ResultDTO confirmRankImport(HttpSession session, @RequestParam Integer confirmID){
+    public ResultDTO confirmRankImport(HttpSession session, @RequestParam Integer confirmId){
         User sessionUser = (User) session.getAttribute("logged_in_as");
         if(sessionUser == null|| !sessionUser.getType().equals("associate")){
             return ResultDTO.errorOf(0, "用户未登录或用户类型不正确。");
         }
         //TODO
-        return null;
+        TempStorageObject<List<SelectRankEntryDTO>> tempStorageObject = (TempStorageObject<List<SelectRankEntryDTO>>) session.getAttribute("last_result_select_ranks_import");
+        if(tempStorageObject == null){
+            return ResultDTO.errorOf(0, "上次上传的文件已经过期，请重新上传。");
+        }
+        if(!tempStorageObject.getId().equals(confirmId)){
+            return ResultDTO.errorOf(0, "确认 ID 不正确，请重新上传文件。");
+        }
+        long count = 0;
+        for(SelectRankEntryDTO selectRankEntryDTO : tempStorageObject.getData()){
+            SelectRankEntry selectRankEntry = new SelectRankEntry();
+            BeanUtils.copyProperties(selectRankEntryDTO, selectRankEntry);
+            count += selectRankEntryMapper.insertSelective(selectRankEntry);
+        }
+        return ResultDTO.okOf(count);
     }
 
 
