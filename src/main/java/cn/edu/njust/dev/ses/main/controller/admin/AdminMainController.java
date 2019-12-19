438736081c710ad3723a910f40cec24e33ecc657
hkai1221 <1119654491@qq.com>
2019-12-20 00:53:41 +0800

    表格的增删操作  修改尚未完成

--- src/main/java/cn/edu/njust/dev/ses/main/controller/admin/AdminMainController.java
+++ src/main/java/cn/edu/njust/dev/ses/main/controller/admin/AdminMainController.java
@@ -1,7 +1,46 @@
 package cn.edu.njust.dev.ses.main.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class AdminMainController {
+    @RequestMapping("/teacher/admin")
+    public String teacherAdmin(HttpSession session){
+        return "teacher/admin";
+    }
+
+    @RequestMapping("/teacher/scoreCheck")
+    public String scoreCheck(HttpSession session){
+        return "teacher/scoreCheck";
+    }
+    @RequestMapping("/teacher/nameListImport")
+    public String nameListImport(HttpSession session){
+        return "teacher/nameListImport";
+    }
+    @RequestMapping("/teacher/Association")
+    public String Association(HttpSession session){
+        return "teacher/Association";
+    }
+    @RequestMapping("/teacher/changePassword")
+    public String changePassword(HttpSession session){
+        return "teacher/changePassword";
+    }
+    @RequestMapping("/teacher/scoreImport")
+    public String scoreImport(HttpSession session){
+        return "teacher/scoreImport";
+    }
+    @RequestMapping("/teacher/scoreList")
+    public String scoreList(HttpSession session){
+        return "teacher/scoreList";
+    }
+    @RequestMapping("/teacher/ccfEvent")
+    public String ccfEvent(HttpSession session){
+        return "teacher/ccfEvent";
+    }
+
+
+
 }
