560fe1765837aa8539031201a2911fd8287805a3
forsaken07 <54091826+forsaken07@users.noreply.github.com>
2019-12-23 20:58:23 +0800

    增加拦截器

--- src/main/java/cn/edu/njust/dev/ses/main/config/SESConfig.java
+++ src/main/java/cn/edu/njust/dev/ses/main/config/SESConfig.java
@@ -1,6 +1,9 @@
 package cn.edu.njust.dev.ses.main.config;
 
 import cn.edu.njust.dev.ses.main.filter.SessionLoginFilter;
+import cn.edu.njust.dev.ses.main.interceptor.AssociationLoginInterceptor;
+import cn.edu.njust.dev.ses.main.interceptor.StudentLoginInterceptor;
+import cn.edu.njust.dev.ses.main.interceptor.TeacherLoginInterceptor;
 import cn.edu.njust.dev.ses.main.listener.SessionCollectionListener;
 import cn.edu.njust.dev.ses.main.service.AccountManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -26,5 +30,25 @@ public class SESConfig implements WebMvcConfigurer {
         frb.addUrlPatterns("/*");
         frb.setOrder(Integer.MIN_VALUE);
         return frb;
+    }
+    @Bean
+    public  WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new AssociationLoginInterceptor())
+                        .excludePathPatterns("/")
+                        .addPathPatterns("/associate/**");
+
+                registry.addInterceptor(new TeacherLoginInterceptor())
+                        .excludePathPatterns("/")
+                        .addPathPatterns("/teacher/**");
+
+                registry.addInterceptor(new StudentLoginInterceptor())
+                        .excludePathPatterns("/")
+                        .addPathPatterns("/student/**");
+            }
+        };
     }
 }
