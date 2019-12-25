df9f514cf32eefe96b355917868072a94cd5fc47
mike0228 <mike990228@gmail.com>
2019-12-25 08:19:53 +0800

    last

---
+++ src/main/resources/db/migration/V28__CREATE_ACCOUNT.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spm_assignment`.`user` (`uid`, `account`, `password`, `type`) VALUES ('1', '1', '123', 'teacher');
+INSERT INTO `spm_assignment`.`teacher` (`uid`, `work_id`, `name`) VALUES ('1', '1', '余立功');
